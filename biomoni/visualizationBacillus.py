846d7aadb178ef4f8dc4805f92ceaf656ee0c197
VBorodina <90382180+VBorodina@users.noreply.github.com>
2022-02-10 10:19:29 +0100

    Start of ne visualization script special for bacillus data

---
+++ biomoni/visualizationBacillus.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append("V:/biomoni/")  
+
+from biomoni import Experiment
+
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+
+
+def visualize(a):
+    pass
